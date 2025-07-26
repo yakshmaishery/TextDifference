7e6fa38d8caae90d2b62b443c638c675b660f4f9
yakshmaishery <yakshmaishery@users.noreply.github.com>
2025-07-26 14:19:57 +0530

    Changed cursor to pointer in dark mode toggle.

--- src/lib/UserComponents/CommonHeader.svelte
+++ src/lib/UserComponents/CommonHeader.svelte
@@ -8,7 +8,7 @@
   <div class="text-xl font-semibold">Text Difference</div>
   <div class="divlinks">
       <a href="https://github.com/yakshmaishery/TextDifference" target="_blank">Github</a>
-     <Button onclick={toggleMode} variant="outline" size="icon">
+     <Button onclick={toggleMode} variant="outline" size="icon" style="cursor:pointer">
          <SunIcon class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 !transition-all dark:-rotate-90 dark:scale-0"/>
          <MoonIcon class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 !transition-all dark:rotate-0 dark:scale-100"/>
          <span class="sr-only">Toggle theme</span>
